2fbce6249e3ddd33f0d57af842f001f54308bb70
Martyna <39212364+mahtyns@users.noreply.github.com>
2021-10-20 23:16:23 +0200

    Add files via upload

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-const words = ["Kotek", "Papuga", "Widelec", "Lampa", "Krzesło", "Astronomia", "Filiżanka", "Szampan", "Krzyżówka", "Wiersz", "Buty", "Nóż", "Mama", "Pas", "Pies"]
+const words = ["Kotek", "Papuga", "Widelec", "Lampa", "Krzesło", "Astronomia", "Filiżanka", "Szampan", "Krzyżówka", "Wiersz", "Buty", "Nóż", "Mama", "Pas", "Pies", "Warszawianin", ]
 
 const hangmanTable = document.querySelector(".hangman-word")
 
@@ -7,20 +7,26 @@ const randomWord = document.querySelector(".randomBtn");
 
 let newWord;
 let wordLetters;
+let randomLetter;
 
-const indices = [];
+// const indices = [];
 
-const dashRandom = () => {
-let dashIndex = hangmanTable.textContent.indexOf("_");
-        while (dashIndex != -1 ) {
-            indices.push(dashIndex);
-            dashIndex = hangmanTable.textContent.indexOf("_", dashIndex + 1);
-        }
-        // console.log(hangmanTable.textContent.length);
-        // console.log(indices);
+// const dashRandom = () => {
+// let dashIndex = hangmanTable.textContent.indexOf("_");
+//         while (dashIndex != -1 ) {
+//             indices.push(dashIndex);
+//             dashIndex = hangmanTable.textContent.indexOf("_", dashIndex + 1);
+//         }
+//         // console.log(hangmanTable.textContent.length);
+//         // console.log(indices);
 
         
        
+// }
+
+// Replace letter function 
+String.prototype.replace = function(index, replaced) {
+   return this.substr(0, index) + replaced + this.substr(index + replaced.length);
 }
 
 const drawWord = () => {
@@ -38,13 +44,16 @@ const hideLetters = () => {
    console.log(newWord);}
 
 const showRandomLetter = () => {
+   
     if (newWord.length <= 5 ) {
-        //Losowanie 1 literki
+        //Drawing 1 letter for short words
         let randomLetterIndex = Math.floor(Math.random() * newWord.length)
-        let randomLetter = newWord[randomLetterIndex];
-        console.log(randomLetter);
-        console.log(randomLetterIndex);
-        dashRandom();
+        randomLetter = newWord[randomLetterIndex];
+        let replacedIndex = 2 * randomLetterIndex + 1
+        hangmanTable.textContent = hangmanTable.textContent.replace(replacedIndex, randomLetter);
+        
+        
+        
         
         //not needed in the end
         // let randomDash = indices[(Math.floor(Math.random() * indices.length))];
@@ -53,10 +62,37 @@ const showRandomLetter = () => {
         
         
     }
-    else {
-        //Losowanie wielu literek
-        console.log("Wiecej niz 5");
+    else if ( 5 < newWord.length && newWord.length <= 10) {
+        //losowanie 2 literek
+        for (let i = 0; i < 2; i++)
+        {
+           let randomLetterIndex = Math.floor(Math.random() * newWord.length);
+           randomLetter = newWord[randomLetterIndex];
+        //    console.log(randomLetter);
+            // console.log(randomLetterIndex);
+            let replacedIndex = 2 * randomLetterIndex + 1
+        hangmanTable.textContent = hangmanTable.textContent.replace(replacedIndex, randomLetter);
+       
+
+
+        }
     }
+    else {
+        for (let i = 0; i < 3; i++)
+        {
+           let randomLetterIndex = Math.floor(Math.random() * newWord.length);
+           randomLetter = newWord[randomLetterIndex];
+        //    console.log(randomLetter);
+            // console.log(randomLetterIndex);
+            let replacedIndex = 2 * randomLetterIndex + 1
+        hangmanTable.textContent = hangmanTable.textContent.replace(replacedIndex, randomLetter);
+               
+
+
+        }
+    }
+
+ 
 }
 
 const showWord = () => {
